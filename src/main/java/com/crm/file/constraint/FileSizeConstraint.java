b5be43f9fdb7c3db1594fc01a380db5c92dcbeb9
Pavel Tseva <paveltseva@gmail.com>
2025-07-28 09:55:01 +0300

    Update default message of FileSizeConstraint

--- src/main/java/com/crm/file/constraint/FileSizeConstraint.java
+++ src/main/java/com/crm/file/constraint/FileSizeConstraint.java
@@ -17,7 +17,7 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 public @interface FileSizeConstraint {
 
-    String message() default "Invalid sign-up request";
+    String message() default "Invalid size of file";
 
     Class<?>[] groups() default {};
 
